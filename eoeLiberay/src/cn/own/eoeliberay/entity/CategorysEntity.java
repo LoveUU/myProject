e2f2b6fc1ac9ed0c64d887da9ad081fd1224e6b1
czdwlai@163.com <czdwlai@163.com>
2013-09-25 16:03:08 +0800

    liberay初次提交

---
+++ eoeLiberay/src/cn/own/eoeliberay/entity/CategorysEntity.java
@@ -0,0 +1,34 @@
+package cn.own.eoeliberay.entity;
+/**
+ * 返回的 大的json的 中的categorys的封装
+ * @author wangxin
+ *
+ */
+public class CategorysEntity {
+	
+	private String name;
+	private String url;
+	
+	public CategorysEntity() {
+		super();
+	}
+	public CategorysEntity(String name, String url) {
+		super();
+		this.name = name;
+		this.url = url;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+
+}
